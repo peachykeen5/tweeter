cc249f3b7756ab11c2641ba9dab1f73a09de533b
peachykeen5 <lexi.keen@gmail.com>
2018-06-19 23:27:57 +0000

    updated styling, added a section containing previous tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,41 @@
+$(document).ready(function () {
+
+    
+});
+
+
+function createTweetElement() //takes a tweet object and is responsible for returning the <article> structure
+
+tweetObject = {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  }
+
+
+
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+ * 
+ * 
+ * Within the app.js file, we're going to define a function createTweetElement that takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.
+
+The tweet data object that the function will take will have all the necessary tweet data:
+ * 
+ * 
+ * 
+ * 
  */
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,16 @@
-$(document).ready(function() {
-    $('textarea').on('keyup', function(){
+$(document).ready(function () {
+    //charCount();
+   $('section.new-tweet textarea').change(charCount);
+   $('section.new-tweet textarea').keyup(charCount);
+});
 
-    })
-
-})
-
-var textArea = document.getElementsByName('text');
-counter.addEventListener('keyup', countChar)
-
-function countChar(){
-}
+function charCount() {
+    var remaining = 140 - $(this).val().length;
+    $(this).parent().find('.counter').html(remaining);
+    if (remaining < 0) {
+        $(this).parent().find('.counter').addClass('red');
+     } else {
+        $(this).parent().find('.counter').removeClass("red")
+     }
+    console.log(this);
+}; 
