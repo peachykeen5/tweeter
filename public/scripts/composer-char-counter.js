d6cb3b6a554c2a7e3096309d814b00fb36ebe55b
peachykeen5 <lexi.keen@gmail.com>
2018-06-19 01:42:02 +0000

    initial commit

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('textarea').on('keyup', function(){
+
+    })
+
+})
+
+var textArea = document.getElementsByName('text');
+counter.addEventListener('keyup', countChar)
+
+function countChar(){
+}
