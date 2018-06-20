400e46aa577e519acd24722dfbebad592e94dbc5
peachykeen5 <lexi.keen@gmail.com>
2018-06-20 23:48:57 +0000

    implemented toggle function and removed hard coded database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,65 +1,16 @@
 $(document).ready(function () {
+    loadTweets();
 
-    const data = [{
-            "user": {
-                "name": "Newton",
-                "avatars": {
-                    "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-                    "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-                    "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-                },
-                "handle": "@SirIsaac"
-            },
-            "content": {
-                "text": "If I have seen further it is by standing on the shoulders of giants"
-            },
-            "created_at": 1461116232227
-        },
-        {
-            "user": {
-                "name": "Descartes",
-                "avatars": {
-                    "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-                    "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-                    "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-                },
-                "handle": "@rd"
-            },
-            "content": {
-                "text": "Je pense , donc je suis"
-            },
-            "created_at": 1461113959088
-        },
-        {
-            "user": {
-                "name": "Johann von Goethe",
-                "avatars": {
-                    "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-                    "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-                    "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-                },
-                "handle": "@johann49"
-            },
-            "content": {
-                "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-            },
-            "created_at": 1461113796368
-        }
-    ];
-
-    //The first function that gets loaded and called
-    renderTweet(data);
-    //console.log(renderTweet(data));
-
-
-    function renderTweet(data) {
-        for (let index of data) {
-            let $tweet = createTweetElement(index);
+//rendering a new tweet and prepending it to the top of the section//
+    function renderTweet(tweets) {
+        $('#tweet-container').empty()
+        tweets.forEach(function(element) {
+            let $tweet = createTweetElement(element);
             $('#tweet-container').prepend($tweet);
-        }
+        })
     };
 
-    //this function creates the structure of the tweet and appends it to the section
+    //this function creates the structure of the tweet and sends it to be rendered
     function createTweetElement(tweet) {
         let $tweet = $('<article>').addClass('tweeter');
         let image = $('<img>').attr('src', tweet.user.avatars.small).addClass('profile'); //creates new image tag with the avatars small object image source
@@ -74,45 +25,46 @@ $(document).ready(function () {
         let icon3 = $('<i>').addClass("fas fa-heart");
         iconSpan.append(icon1, icon2, icon3);
         let $footer = $('<footer>').append(tweet.created_at, iconSpan);
-        $tweet.append($header, $tweetBody, $footer); //adds the header, body, and footer to the article
-        //console.log(createTweetElement(data));
-        return ($tweet);
+        $tweet.append($header, $tweetBody, $footer);
+        return $tweet;
     }
 
-
-    $('form').on('submit', function (e) {
+    $('#tweet-compose-form').on('submit', function (e) {
         e.preventDefault();
         //gets the data entered into the form and sends it as a serialized string to our //database using the POST method.
-        let data = $('form').serialize();
-        let counter = $('form textarea').val()
+        let data = $(this).serialize();
+        let counter = e.target[0].value;
         if (counter.length <= 0) {
             alert('Please enter some text')
+        }
+        if (counter.length > 140) {
+            alert('This tweet is too long')
         } else {
-            if (counter.length > 140) {
-                alert('This tweet is too long')
-            } else {
-                $.ajax('/tweets/', {
-                    method: 'POST',
-                    data: data
-                }).done(function (res) {
-                    loadTweets(res);
-                    $('form textarea').val(''); //clears text area
-                })
-            }
+            $.ajax('/tweets', {
+                method: 'POST',
+                data: data
+            }).done(function (res) {
+                loadTweets(res);
+                $('.textarea').val(''); //clears text area
+            })
         }
     });
 
-
-    ////loadTweets is responsible for fetching tweets from the http://localhost:8080/tweets //page.
     function loadTweets() {
         $.ajax({
-            url: '/tweets/', //makes a request with jQuery to /tweets to receive array  //tweets as JSON
+            url: '/tweets',
             method: 'GET',
             success: function (data) {
-
                 renderTweet(data);
             }
         })
     }
+
+    //function to hide/show compose tweet section 
+    $('#nav-bar button').on('click', function(){
+        $('.new-tweet').slideToggle('slow', function(){
+        $('textarea').focus();
+        })
+    })
 
 });
