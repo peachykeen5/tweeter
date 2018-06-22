e19c01abbcdcc29b12ae5ae61e395b2565c00423
peachykeen5 <lexi.keen@gmail.com>
2018-06-22 23:53:45 +0000

    mongoDB

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ MongoClient.connect(MONGOB_URI, (err, client) => {
     console.error(`Failed to connect:${MONGOB_URI}`);
     throw err;
   }
-  const db = client.db('tweeter');
+  const db = client.db(MONGOB_URI);
   console.log(`Connected to mongodb: ${MONGOB_URI}`);
 
   const DataHelpers = require("./lib/data-helpers.js")(db);
